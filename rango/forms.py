e9ec65e75edd6f894e3e5caec3b7b1fdbbbc6d2f
Ciara Losel <2438870l@student.gla.ac.uk>
2021-02-07 13:29:25 +0000

    Chapter 7 completed

---
+++ rango/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+	name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+	#Inline class provides additional info on form.
+	class Meta:
+		#Provide association between ModelForm and model
+		model = Category
+		fields = ('name',)
+
+class PageForm(forms.ModelForm):
+	title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+	url = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter the URL of the page.")		
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+	class Meta:
+		#Provide association between the ModelForm and a model
+		model = Page
+
+		#What fields do we want to include?
+		#This way we don't need every field in model present.
+		#Some fields may allow null values as we might not want to include them
+		#Here we're hiding foreign key, we can either exclude category field from form:
+		exclude = ('category',)
+		#or specify fields to include without category...
+		#fields = ('title', 'url', 'views')
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+
+		#If url not empty and doesn't start with 'http://' then prepend it
+		if url and not url.startswith('http://'):
+			url = f'http://{url}'
+			cleaned_data['url'] = url
+		
+		return cleaned_data
--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
 
 def index(request):
 	#Query database for list of all categories currently stored ordered by descending number of likes
@@ -48,4 +52,57 @@ def show_category(request, category_name_slug):
 
 	#Render response and return to client
 	return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+	form = CategoryForm()
+	
+	#A HTTP POST?
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		#Have we been provided with valid form?
+		if form.is_valid():
+			#Save new category to database
+			form.save(commit=True)
+			#Now category is saved we could confirm this, but for now just redirect user back to index view
+			return redirect('/rango/')
+		else:
+			#Supplied form contained errors so print them to terminal
+			print(form.errors)
+
+	#Will handle bad form, new form or no form.
+	#Render form with error messages if any
+	return render(request, 'rango/add_category.html', {'form': form})
+	
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	#Can't add page to category that doesn't exist
+	if category is None:
+		return redirect('/rango/')
+
+	form = PageForm()
+	
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+
+		#Have we been provided with valid form?
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+		else:
+			#Supplied form contained errors so print them to terminal
+			print(form.errors)
+
+	context_dict = {'form': form, 'category': category}
+	#Will handle bad form, new form or no form.
+	#Render form with error messages if any
+	return render(request, 'rango/add_page.html', context=context_dict)
 
